c7042e7c5490f1cbca8c84c5c3b7a816e75f0994
den413 <rsbyg390@yahoo.co.jp>
2023-09-16 16:22:43 +0900

    change axios url

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5050/api/v1";
+// const BASE_URL = "http://localhost:5050/api/v1";
+const BASE_URL = "https://notion-clone-server.vercel.app/api/v1";
 
 const getToken = () => localStorage.getItem("token");
 const axiosClient = axios.create({
